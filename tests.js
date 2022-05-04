c1ab3f7f814b33275756b7d5aa198e565705a0c6
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2022-05-04 00:59:52 -0500

    test base class

--- tests.js
+++ tests.js
@@ -5,8 +5,8 @@ let derivedClass2TestElement = document.querySelector("#derived-class-2-test")
 //wrap everything in try catch since they won't have classes to begin with
 try {
     let v = new Vehicle("Jord J-051")
-    if(v.model === "Jord J-051"){
-        baseClassTestElement.textContent = " Passed"
+    if(v.model === "Jord J-051" && v.drive() === "Jord J-051 drives"){
+        baseClassTestElement.textContent = "Passed"
         baseClassTestElement.style.color = "green"
     }
 } catch (error) {
